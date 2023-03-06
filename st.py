2e72dff842d3f0ba6e9f344ca58675b7b44d78d8
pyxel <morboagrees@gmail.com>
2023-03-06 21:22:19 +0000

    Connect to SF

--- st.py
+++ st.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import snowflake.connector
+import os
+
+
+def connect(account, user):
+    
+    conn = snowflake.connector.connect(
+        user=user,
+        account=account,
+        authenticator = 'externalbrowser'
+        #warehouse=WAREHOUSE,
+        #database=DATABASE,
+        #schema=SCHEMA
+        )
+
+    (col1,) = conn.cursor().execute("select 'Hello from Snowflake' col").fetchone()
+    st.write(col1)
+
 
 st.write('This is streamlit.')
+
+account = st.text_input(label = "account", value = "")
+user = st.text_input(label = "user", value = "")
+btn_connect = st.button(label = "Connect", on_click=connect, args=(account, user))
